168484420a00a7879f7dc2866bc98ecaa588dd15
Judah <judahtrembe@gmail.com>
2020-12-25 15:47:30 +0100

    database problems

--- venv/project/routes.py
+++ venv/project/routes.py
@@ -32,8 +32,6 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
-    def __repr__(self):
-        return '<User %r>' % self.id
 
 
 class Todo(db.Model):
