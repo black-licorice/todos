9265aa92c5774d26a544df50c06517f8b923d0bb
Judah <judahtrembe@gmail.com>
2020-12-27 19:13:36 +0100

    fix filepaths

--- venv/clock.py
+++ venv/clock.py
@@ -10,12 +10,12 @@ sched = BlockingScheduler()
 @sched.scheduled_job('interval', seconds=30)
 def timed_job():
     time = datetime.datetime.utcnow()
-    for line in open('todos.csv', 'r'):
+    for line in open('project/todos.csv', 'r'):
         email_date = line.split(',')[1][1:].split(' ')[0]
         if email_date == str(time).split(' ')[0]:
             email_time= line.split(',')[1][1:].split(' ')[1][0:5]
             if email_time == time.strftime('%H:%M'):
-                for user in open('users.csv', 'r'):
+                for user in open('project/users.csv', 'r'):
                     if line.split(',')[0] == user.split(',')[0][1:]:
                         user_email = user.split(',')[1]
                         print(user_email)
