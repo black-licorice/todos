b5745a22f2ca3fad45296b9fb0751b54b755a3fc
Judah <judahtrembe@gmail.com>
2020-12-29 14:34:46 +0100

    debug

--- venv/project/routes.py
+++ venv/project/routes.py
@@ -90,6 +90,7 @@ def timed_email():
     time = datetime.datetime.now()
     print('starting timed email at: ', time)
     for todo in db.session.query(Todo).filter_by(email_me=True).all():
+        print(todo)
         if str(todo.email_date).split(',')[0][0:10] == str(time).split(' ')[0]:
             email_time = str(todo.email_date).split(',')[0].split(' ')[1][0:5]
             if email_time == time.strftime('%H:%M'):
