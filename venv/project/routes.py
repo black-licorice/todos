cb2ddd28a3042351993f8c0c9b039c5159eb3e6a
Judah <judahtrembe@gmail.com>
2020-12-29 16:22:22 +0100

    typo

--- venv/project/routes.py
+++ venv/project/routes.py
@@ -88,7 +88,7 @@ def index_post():
 
 
 def timed_email():
-    time = datetime.datetime.now().astimezone(pytz.utc)
+    time = datetime.datetime.now()
     for todo in db.session.query(Todo).filter_by(email_me=True).all():
         if str(todo.email_date).split(',')[0][0:10] == str(time).split(' ')[0]:
             email_time = str(todo.email_date).split(',')[0].split(' ')[1][0:5]
